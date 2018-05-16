fac51e2ff7227732645213b7e53ef78f2edf8b49
Hamzautd7 <hamza.utd7@gmail.com>
2018-05-17 01:55:37 +0500

    get post postman

---
+++ server/models/Todo.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', ({
+    text: {
+        type: String,
+        required: true,
+        minlength: 3,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: true
+    },
+    completedAt:{
+        type: Number
+    } 
+}));
+module.exports = {Todo};
